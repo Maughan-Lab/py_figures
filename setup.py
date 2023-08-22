9a3f4fafb71bd1276c4b2f2b30aa59b8009a8b6f
sinclaircombs <90660007+sinclaircombs@users.noreply.github.com>
2023-08-22 15:40:44 -0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+   name='py_figures',
+   version='0.1',
+   author='Sinclair Combs',
+   author_email='sinclaircombs@mines.edu',
+   packages=['py_figures']
+)
