da1f271dc12d9931a852b9dd117399284203eaf4
sinclaircombs <90660007+sinclaircombs@users.noreply.github.com>
2023-08-24 09:49:11 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
    name='py_figures',
    url='https://github.com/Maughan-Lab/py_figures',
-   version='0.9.1',
+   version='0.9.2',
    author='Sinclair R. Combs',
    author_email='sinclaircombs@mines.edu',
    packages=['py_figures'],
