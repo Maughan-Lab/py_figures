1ad47012ef450321085543c1005a1fca9c37da0e
sinclaircombs <90660007+sinclaircombs@users.noreply.github.com>
2023-08-24 10:30:12 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(
    name='py_figures',
    url='https://github.com/Maughan-Lab/py_figures',
-   version='0.9.6',
+   version='1.0',
    author='Sinclair R. Combs',
    author_email='sinclaircombs@mines.edu',
    packages=['py_figures'],
-   description='Py Figures'
+   description='Python package for generate XRD, PDF, EIS, and CP plots'
 )
